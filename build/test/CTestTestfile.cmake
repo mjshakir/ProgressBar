622f2876a0622532d5f9f1c410a798faa3687d1f
mjshakir <maslove23@gmail.com>
2023-11-02 16:03:11 -0300

    remove

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/majed/Documents/Project/ProgressBar/test
-# Build directory: /home/majed/Documents/Project/ProgressBar/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ProgressBar_unit_tests "/home/majed/Documents/Project/ProgressBar/build/bin/ProgressBar_tests")
-set_tests_properties(ProgressBar_unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/majed/Documents/Project/ProgressBar/test/CMakeLists.txt;34;add_test;/home/majed/Documents/Project/ProgressBar/test/CMakeLists.txt;0;")
