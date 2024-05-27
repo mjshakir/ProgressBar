dacaf95c97e5320a4f2eb8238e130fdc0caa520e
mjshakir <maslove23@gmail.com>
2024-05-26 22:07:54 -0700

    Change: minor formating

--- include/ProgressBar.hpp
+++ include/ProgressBar.hpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <chrono>
 //--------------------------------------------------------------
-namespace ProgressBar{
+namespace ProgressBar {
     //--------------------------------------------------------------
     /**
      * @class ProgressBar
